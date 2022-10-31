01174de5246563ce17901848a9cabb57034a4887
shreeja-shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-10-31 19:19:52 +0100

    Update resistor_color.py

--- resistor_color.py
+++ resistor_color.py
@@ -1,6 +1,31 @@
 def color_code(color):
-    pass
+    y=(color.lower())
+    if(y=="black"):
+        return 0
+    elif(y=="brown"):
+        return 1
+    elif(y=="red"):
+        return 2
+    elif(y=="orange"):
+        return 3
+    elif(y=="yellow"):
+        return 4
+    elif(y=="green"):
+        return 5
+    elif(y=="blue"):
+        return 6
+    elif(y=="violet"):
+        return 7
+    elif(y=="grey"):
+        return 8
+    elif(y=="white"):
+        return 9
+    else:
+        return "RaiseValueError"
+    
+    
 
 
 def colors():
-    pass
+    x={"black","brown","red","orange","yellow","green","blue","violet","grey","white",}
+return x
